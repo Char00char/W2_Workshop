65fbfd4e6a710a0bb6c5365739aa09b5b13c35c1
astroalicea <chonny507@gmail.com>
2026-04-17 11:00:38 -0400

    alicea_Ex3A create tables complete

--- alicea_Ex3A_create_tables.sql
+++ alicea_Ex3A_create_tables.sql
@@ -1,3 +1,25 @@
 DROP SCHEMA IF EXISTS lana_dog_walking;
 CREATE SCHEMA lana_dog_walking;
 USE lana_dog_walking;
+
+CREATE TABLE Walkers (
+	walker_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100),
+    phone VARCHAR(20),
+    role VARCHAR(30),
+    hire_date DATE
+);
+CREATE TABLE Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    phone VARCHAR(20),
+    email VARCHAR(100),
+    address VARCHAR(150),
+    city VARCHAR(50),
+    state VARCHAR(2),
+    zip VARCHAR(10),
+    emergency_contact VARCHAR(100)
+);
